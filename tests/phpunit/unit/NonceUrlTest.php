5ca2281db96431cbf327441cb2fbc625a12a3417
ChriCo <family.brueckner@googlemail.com>
2016-02-26 11:43:09 +0100

    added missing getMock()

--- tests/phpunit/unit/NonceUrlTest.php
+++ tests/phpunit/unit/NonceUrlTest.php
@@ -3,6 +3,7 @@
 namespace Inpsyde\Tests\Nonces;
 
 use Brain\Monkey;
+use Mockery;
 use Inpsyde\Nonces\NonceUrl as Testee;
 use PHPUnit_Framework_TestCase;
 
@@ -16,13 +17,16 @@ class NonceUrlTest extends PHPUnit_Framework_TestCase {
 		Monkey\Functions::expect( 'wp_create_nonce' )
 		                ->andReturn( '123' );
 
-		$context = Mockery::mock( 'Inpsyde\Nonces\Context' );
-		$context->shouldReceive( 'get_action' )
-		        ->andReturn( 'Meine Action' );
-		$context->shouldReceive( 'get_name' )
-		        ->andReturn( 'nonce' );
+		$context = Mockery::mock( 'Inpsyde\Nonces\Context' )
+		                  ->shouldReceive( 'get_action' )
+		                  ->andReturn( 'Meine Action' )
+		                  ->shouldReceive( 'get_name' )
+		                  ->andReturn( 'nonce' )
+		                  ->getMock();
 
-		$this->assertEquals( 'http://inpsyde.com/?nonce=123',
-		                     $testee->add_query_arg( 'http://inpsyde.com', $context ) );
+		$this->assertEquals(
+			'http://inpsyde.com/?nonce=123',
+			$testee->add_query_arg( 'http://inpsyde.com', $context )
+		);
 	}
 }
