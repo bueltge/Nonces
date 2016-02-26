73259125da9f10f77291a64f6812de433b728304
tinabillinger <tina@billinger.com>
2016-02-26 14:02:15 +0100

    Added package tag

--- src/Output/FormField.php
+++ src/Output/FormField.php
@@ -2,6 +2,12 @@
 
 namespace Inpsyde\Nonces\Output;
 
+/**
+ * FormField creator.
+ *
+ * @package Inpsyde\Nonces
+ */
+
 class FormField {
 
   /**
@@ -12,7 +18,7 @@ class FormField {
    * @return string
    */
 
-   public function get( Context $context ) {
+   public function get ( Context $context ) {
 
      return wp_nonce_field( $context->get_action(), $context->get_name() );
    }
