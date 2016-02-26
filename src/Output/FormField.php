e24ccf6d028e9373cf66f7b0d1584e39258de3a5
tinabillinger <tina@billinger.com>
2016-02-26 13:45:08 +0100

    Added FormField Class

--- src/Output/FormField.php
+++ src/Output/FormField.php
@@ -1,7 +1,31 @@
 <?php # -*- coding: utf-8 -*-
 
-namespace Inpsyde\Nonces\Output;
+//namespace Inpsyde\Nonces\Output;
 
 class FormField {
+
+  /**
+   * Returns a hidden field with the nonce parameter added.
+   *
+   * @param Context $context
+   *
+   * @return string
+   */
+
+   public function get( Context $context ) {
+
+     return wp_nonce_field( $context->get_action(), $context->get_name() );
+   }
+
+   /**
+    * Prints a hidden field with the nonce parameter added.
+    *
+    * @return string
+    */
+
+   public function render ( Context $context ) {
+
+     echo $this->get( $context );
+   }
 
 }
