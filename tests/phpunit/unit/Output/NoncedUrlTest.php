7dc1145afd6d12b53c789c619ece60e63733d506
tfrommen <info@tfrommen.de>
2016-02-26 18:15:03 +0100

    Move and update Nonce(d)Url class and test.

---
+++ tests/phpunit/unit/Output/NoncedUrlTest.php
@@ -0,0 +1,65 @@
+<?php # -*- coding: utf-8 -*-
+
+namespace Inpsyde\Tests\Nonces\Output;
+
+use Brain\Monkey;
+use Inpsyde\Nonces\Context;
+use Inpsyde\Nonces\Output\NoncedUrl as Testee;
+use Inpsyde\Tests\Nonces\TestCase;
+use Mockery;
+
+/**
+ * Test case for the NoncedUrl class.
+ *
+ * @package Inpsyde\Tests\Nonces\Output
+ */
+class NoncedUrlTest extends TestCase {
+
+	/**
+	 * Test for the get() method.
+	 *
+	 * @return void
+	 */
+	public function test_get() {
+
+		$testee = new Testee();
+
+		$nonced_url = 'nonced_url';
+
+		Monkey\Functions::when( 'wp_nonce_url' )
+			->justReturn( $nonced_url );
+
+		/** @var Context $context */
+		$context = Mockery::mock( 'Inpsyde\Nonces\Context' )
+			->shouldReceive( 'get_action' )
+			->shouldReceive( 'get_name' )
+			->getMock();
+
+		$this->assertSame( $nonced_url, $testee->get( 'url', $context ) );
+	}
+
+	/**
+	 * Test for the render() method.
+	 *
+	 * @return void
+	 */
+	public function test_render() {
+
+		$testee = new Testee();
+
+		$nonced_url = 'nonced_url';
+
+		Monkey\Functions::when( 'wp_nonce_url' )
+			->justReturn( $nonced_url );
+
+		/** @var Context $context */
+		$context = Mockery::mock( 'Inpsyde\Nonces\Context' )
+			->shouldReceive( 'get_action' )
+			->shouldReceive( 'get_name' )
+			->getMock();
+
+		$this->expectOutputString( $nonced_url );
+
+		$testee->render( 'url', $context );
+	}
+}
