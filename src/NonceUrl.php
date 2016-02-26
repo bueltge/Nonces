f682db3ec1a12e999a321c2f3a2575e32bc0e979
tfrommen <info@tfrommen.de>
2016-02-26 16:57:31 +0100

    Update NonceUrl class.

--- src/NonceUrl.php
+++ src/NonceUrl.php
@@ -5,15 +5,15 @@ namespace Inpsyde\Nonces;
 class NonceUrl {
 
 	/**
-	 * Returns the URL with the nonce parameter added.
+	 * Returns the given URL with a nonce parameter added.
 	 *
-	 * @param string  $url
-	 * @param Context $context
+	 * @param string  $url     The current URL.
+	 * @param Context $context The nonce context data object.
 	 *
 	 * @return string
 	 */
 	public function add_query_arg( $url, Context $context ) {
 
-		return wp_nonce_url( $url, $context->get_action(), $context->get_name() );
+		return wp_nonce_url( (string) $url, $context->get_action(), $context->get_name() );
 	}
 }
