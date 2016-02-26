0b9557553a6ccdffa040d73f8738296e47d0bdcf
ChriCo <family.brueckner@googlemail.com>
2016-02-26 11:46:10 +0100

    added missing mock for sanitize_title_with_dashes

--- tests/phpunit/unit/ContextFactoryTest.php
+++ tests/phpunit/unit/ContextFactoryTest.php
@@ -2,12 +2,15 @@
 
 namespace Inpsyde\Tests\Nonces;
 
+use Brain\Monkey;
 use Inpsyde\Nonces\ContextFactory as Testee;
 use PHPUnit_Framework_TestCase;
 
 class ContextFactoryTest extends PHPUnit_Framework_TestCase {
 
 	public function test_basiccreate() {
+
+		Monkey\Functions::when( '\sanitize_title_with_dashes' );
 
 		$testee = new Testee();
 
